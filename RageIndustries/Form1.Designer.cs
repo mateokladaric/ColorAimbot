67189130d0d3c6b5a39fe5015dcaad584220c573
MrFacysus <52569856+MrFacysus@users.noreply.github.com>
2021-03-10 10:32:01 +0100

    Final Release

---
+++ RageIndustries/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace RageIndustries
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightCoral;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(662, 59);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mind Boggle DEMO", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(603, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 50);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(39, 37);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mind Boggle DEMO", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(57, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(343, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rage Industries";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(480, 331);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "XMultiplier: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(480, 357);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "YMultiplier: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(547, 328);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "4";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(547, 354);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "4";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(480, 301);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Sens:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(547, 298);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "100";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Mind Boggle DEMO", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(645, 387);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "This Tool is made by MrFacysus\r\nJust run this and go into your game\r\ndefault butt" +
+    "on is XButton2!\r\nTo contact me go on discord\r\nand add Mr Facysus#4030\r\n\r\nBTW Cli" +
+    "ck on this text\r\n\r\n\r\n";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(660, 387);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ RageIndustries/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace RageIndustries
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("User32.dll")]
+        public static extern short GetAsyncKeyState(Keys vKey);
+
+        [DllImport("User32.dll")]
+        public static extern long SetCursorPos(int x, int y);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        public static int ScreenXReq = 110;
+        public static int ScreenYReq = 110;
+
+        Bitmap screenshot = new Bitmap(ScreenXReq, ScreenYReq);
+        Bitmap screenshot2 = new Bitmap(ScreenXReq, ScreenYReq);
+
+        Graphics graphics;
+
+        public int MissX;
+        public int MissY;
+
+        public int[] CompareBitmapsLazy(Bitmap bmp1, Bitmap bmp2)
+        {
+            //Target X i Y
+            int[] output = new int[2] { 0, 0 };
+
+            for (int x = 2; x < bmp1.Width-2; x = x + 4)
+            {
+                for (int y = 2 ; y < bmp1.Height-2; y = y + 4)
+                {
+                    int RDif = Math.Abs(bmp1.GetPixel(x, y).R - bmp2.GetPixel(x, y).R);
+                    int GDif = Math.Abs(bmp1.GetPixel(x, y).G - bmp2.GetPixel(x, y).G);
+                    int BDif = Math.Abs(bmp1.GetPixel(x, y).B - bmp2.GetPixel(x, y).B);
+
+                    int R1Dif = Math.Abs(bmp1.GetPixel(x + 1, y).R - bmp2.GetPixel(x, y).R);
+                    int G1Dif = Math.Abs(bmp1.GetPixel(x + 1, y).G - bmp2.GetPixel(x, y).G);
+                    int B1Dif = Math.Abs(bmp1.GetPixel(x + 1, y).B - bmp2.GetPixel(x, y).B);
+
+                    int R2Dif = Math.Abs(bmp1.GetPixel(x, y + 1).R - bmp2.GetPixel(x, y).R);
+                    int G2Dif = Math.Abs(bmp1.GetPixel(x, y + 1).G - bmp2.GetPixel(x, y).G);
+                    int B2Dif = Math.Abs(bmp1.GetPixel(x, y + 1).B - bmp2.GetPixel(x, y).B);
+
+                    int R3Dif = Math.Abs(bmp1.GetPixel(x, y - 1).R - bmp2.GetPixel(x, y).R);
+                    int G3Dif = Math.Abs(bmp1.GetPixel(x, y - 1).G - bmp2.GetPixel(x, y).G);
+                    int B3Dif = Math.Abs(bmp1.GetPixel(x, y - 1).B - bmp2.GetPixel(x, y).B);
+
+                    int R4Dif = Math.Abs(bmp1.GetPixel(x - 1, y).R - bmp2.GetPixel(x, y).R);
+                    int G4Dif = Math.Abs(bmp1.GetPixel(x - 1, y).G - bmp2.GetPixel(x, y).G);
+                    int B4Dif = Math.Abs(bmp1.GetPixel(x - 1, y).B - bmp2.GetPixel(x, y).B);
+
+
+                    int Dif = Math.Abs((RDif + GDif + BDif + R1Dif + R2Dif +R3Dif + R4Dif + G1Dif + G2Dif + G3Dif + G4Dif + B1Dif + B2Dif + B3Dif + B4Dif) / 15);
+
+                    int Sens = Int32.Parse(textBox3.Text);
+
+                    if (Dif > Sens)
+                    {
+                        output[0] = x;
+                        output[1] = y;
+                    }
+                }
+            }
+            return output;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread Start = new Thread(Main);
+            Start.IsBackground = true;
+            Start.Start();
+        }
+
+        private void Main()
+        {
+            while (true)
+            {
+                if (GetAsyncKeyState(Keys.XButton1) < 0)
+                {
+                    graphics = Graphics.FromImage(screenshot as Image);
+                    int SourceX = (Screen.PrimaryScreen.Bounds.Width / 2) - 55;
+                    int SourceY = (Screen.PrimaryScreen.Bounds.Height / 2) - 55;
+                    int DestX = 0;
+                    int DestY = 0;
+                    System.Drawing.Size ImgSize = new System.Drawing.Size(ScreenXReq, ScreenYReq);
+                    graphics.CopyFromScreen(SourceX, SourceY, DestX, DestY, ImgSize);
+                    Bitmap PicBefore = screenshot;
+                    Thread.Sleep(10);
+                    graphics = Graphics.FromImage(screenshot2 as Image);
+                    graphics.CopyFromScreen(SourceX, SourceY, DestX, DestY, ImgSize);
+                    Bitmap PicAfter = screenshot2;
+
+                    int[] Target = CompareBitmapsLazy(PicBefore, PicAfter);
+
+                    if (Target[0] != 0 & Target[1] != 0)
+                    {
+                        Target[1] = Target[1] - 55;
+                        Target[0] = Target[0] - 55;
+                        mouse_event(0x0001, Target[0]*Int32.Parse(textBox1.Text), Target[1]*Int32.Parse(textBox2.Text), 0, 0);
+                        Thread.Sleep(10);
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        Thread.Sleep(5);
+                        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                        Thread.Sleep(200);
+                    }
+                }
+            }
+        }
+        private bool mouseDown;
+        private System.Drawing.Point lastLocation;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new System.Drawing.Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com/channel/UCiddpR_hZ92qqpioZn4qUhw");
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            return;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
